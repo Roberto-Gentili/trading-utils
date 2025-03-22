338141c19ae67472dba1c60355ef9f6bd1733863
Roberto Gentili <roberto.gentili.1980@gmail.com>
2025-03-22 09:58:18 +0100

    Graphical improvment

--- src/main/java/org/rg/service/Asset.java
+++ src/main/java/org/rg/service/Asset.java
@@ -18,10 +18,10 @@ import org.ta4j.core.Bar;
 import org.ta4j.core.BarSeries;
 
 public class Asset {
-	private static String NOT_AVAILABLE = "<center><i style=\"color: #D3D3D3;\">na</i></center>";
+	private static String NOT_AVAILABLE = "<center><i style=\"color: #C8C8C8;\">na</i></center>";
 	private static String EVEN_ROW_BACKGROUND_COLOR = "#D6EEEE";
 	private static String ODD_ROW_BACKGROUND_COLOR = "#F8F8F8";
-	private static String HEADER_ROW_COLOR = "#7393B3";
+	private static String HEADER_ROW_STYLE = "background-color: #7393B3; color: white;";
 	private static String CELL_PADDING ="15px";
 	private Map<String, Object> values;
 
@@ -191,7 +191,7 @@ public class Asset {
 				}
 			}
 			return "<table style=\"border-spacing: 0px;font-size:" + Application.mailFontSizeInPixel + ";\">" +
-				"<tr style=\"background-color:" + HEADER_ROW_COLOR + "\">" +
+				"<tr style=\"" + HEADER_ROW_STYLE + "\">" +
 					String.join("", labels.stream().filter(hideColumnFilter()).map(label -> "<td style=\"padding: " + CELL_PADDING + "\"><b>" + label + "</b></td>").collect(Collectors.toList())) +
 					String.join("", dynamicLabelsGroupOne.stream().map(label -> "<td style=\"padding: " + CELL_PADDING + "\"><b>" + label + "</b></td>").collect(Collectors.toList())) +
 					String.join("", dynamicLabelsGroupTwo.stream().map(label -> "<td style=\"padding: " + CELL_PADDING + "\"><b>" + label + "</b></td>").collect(Collectors.toList())) +
