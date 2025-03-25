fc12f7cd33db6489f6fc4156e01e24633fa1f57d
Roberto Gentili <roberto.gentili.1980@gmail.com>
2025-03-25 09:49:41 +0100

    Improved code

--- src/main/java/org/rg/service/Application.java
+++ src/main/java/org/rg/service/Application.java
@@ -489,19 +489,19 @@ public class Application implements CommandLineRunner {
 					}
 				}
 			}
-			if (ShowConsistentDataOption.HIGHLIGHT_THEM.valueEquals(showConsistentDataOption)) {
-				if ((alreadyNotifiedGreenUpdaters.isEmpty() && !alreadyNotifiedRedUpdaters.isEmpty()) ||
-					(alreadyNotifiedRedUpdaters.isEmpty() && !alreadyNotifiedGreenUpdaters.isEmpty())) {
-					asset.convertNameToColored();
-				}
-				alreadyNotifiedUpdaters.addAll(allUpdaters);
-			} else if (ShowConsistentDataOption.ONLY_THEM.valueEquals(showConsistentDataOption)) {
+			if (ShowConsistentDataOption.ONLY_THEM.valueEquals(showConsistentDataOption)) {
 				if (alreadyNotifiedGreenUpdaters.isEmpty()) {
 					alreadyNotifiedUpdaters.addAll(alreadyNotifiedRedUpdaters);
 				} else if (alreadyNotifiedRedUpdaters.isEmpty()) {
 					alreadyNotifiedUpdaters.addAll(alreadyNotifiedGreenUpdaters);
 				}
 			} else {
+				if (ShowConsistentDataOption.HIGHLIGHT_THEM.valueEquals(showConsistentDataOption)) {
+					if ((alreadyNotifiedGreenUpdaters.isEmpty() && !alreadyNotifiedRedUpdaters.isEmpty()) ||
+						(alreadyNotifiedRedUpdaters.isEmpty() && !alreadyNotifiedGreenUpdaters.isEmpty())) {
+						asset.convertNameToColored();
+					}
+				}
 				alreadyNotifiedUpdaters.addAll(allUpdaters);
 			}
 		}
