cf99a55e5447ee2fbfb5f43c4c44b1d4775d3f07
Roberto Gentili <roberto.gentili.1980@gmail.com>
2025-03-25 14:14:01 +0100

    Bug fix

--- src/main/java/org/rg/service/Application.java
+++ src/main/java/org/rg/service/Application.java
@@ -250,7 +250,6 @@ public class Application implements CommandLineRunner {
 					Collection<String> marginUSDCCoins = ((BinanceWallet)walletForAvailableCoins.getKey()).getAllMarginAssetPairs()
 						.stream().filter(asset -> asset.get("quote").equals(defaultCollateral)).map(asset -> asset.get("base")).
 						map(String.class::cast).collect(Collectors.toList());
-					marginUSDCCoins= Arrays.asList("BTC", "CRV");
 					marginUSDCCoins.parallelStream().forEach(coin -> {
 						try {
 							org.burningwave.core.assembler.StaticComponentContainer.ManagedLoggerRepository.logInfo(
