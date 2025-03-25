414e1c0140539c959fb2a47ec8648c367103a1f5
Roberto Gentili <roberto.gentili.1980@gmail.com>
2025-03-25 13:14:13 +0100

    Bug fix

--- src/main/java/org/rg/service/Asset.java
+++ src/main/java/org/rg/service/Asset.java
@@ -292,9 +292,9 @@ public class Asset {
 
 		private String toHTML(List<String> header, Asset data, int rowCounter, boolean onTopFixedHeader) {
 			return
-				(onTopFixedHeader ? "" :
+				(!onTopFixedHeader ? "" :
 					"<tr style=\"" + HEADER_ROW_STYLE + "\">" +
-						String.join("", header.stream().filter(showColumnFilter()).map(label -> "<th style=\"" + HEADER_CELL_STYLE + "\"><b>" + label + "</b></th>").collect(Collectors.toList())) +
+						String.join("", header.stream().filter(showColumnFilter()).map(label -> "<td style=\"" + HEADER_CELL_STYLE + "\"><b>" + label + "</b></td>").collect(Collectors.toList())) +
 					"</tr>"
 				) +
 				"<tr style=\"" + (rowCounter % 2 == 0 ? EVEN_ROW_STYLE : ODD_ROW_STYLE) + "\">" +
