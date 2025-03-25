5250f5ce09ac620e253c8a85d61c52c0da406295
Roberto Gentili <roberto.gentili.1980@gmail.com>
2025-03-25 14:18:20 +0100

    Update

--- setup-ssh.cmd
+++ setup-ssh.cmd
@@ -2,17 +2,17 @@
 ::# Set Key File Variable:
 Set Key="%UserProfile%\.ssh\id_rsa"
 ::# Remove Inheritance:
-Icacls %Key% /c /t /Inheritance:d
+Icacls "%Key%" /c /t /Inheritance:d
 ::# Set Ownership to Owner:
 :: # Key's within %UserProfile%:
-Icacls %Key% /c /t /Grant %UserName%:F
+Icacls "%Key%" /c /t /Grant %UserName%:F
 :: # Key's outside of %UserProfile%:
 TakeOwn /F %Key%
-Icacls %Key% /c /t /Grant:r %UserName%:F
+Icacls "%Key%" /c /t /Grant:r %UserName%:F
 ::# Remove All Users, except for Owner:
-Icacls %Key% /c /t /Remove:g "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
+Icacls "%Key%" /c /t /Remove:g "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
 ::# Verify:
-Icacls %Key%
+Icacls "%Key%"
 ::# Remove Variable:
 set "Key="
 
