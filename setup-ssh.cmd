503329f65086229fa37d698c876f8832df7854b2
Roberto Gentili <roberto.gentili.1980@gmail.com>
2025-03-25 14:29:39 +0100

    Updated asset report

--- setup-ssh.cmd
+++ setup-ssh.cmd
@@ -5,10 +5,10 @@ Set Key="%UserProfile%\.ssh\id_rsa"
 Icacls "%Key%" /c /t /Inheritance:d
 ::# Set Ownership to Owner:
 :: # Key's within %UserProfile%:
-Icacls "%Key%" /c /t /Grant %UserName%:F
+Icacls "%Key%" /c /t /Grant "%UserName%":F
 :: # Key's outside of %UserProfile%:
 TakeOwn /F %Key%
-Icacls "%Key%" /c /t /Grant:r %UserName%:F
+Icacls "%Key%" /c /t /Grant:r "%UserName%":F
 ::# Remove All Users, except for Owner:
 Icacls "%Key%" /c /t /Remove:g "Authenticated Users" BUILTIN\Administrators BUILTIN Everyone System Users
 ::# Verify:
